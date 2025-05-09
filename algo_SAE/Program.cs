21d9b7b449f8f6127ca7d19ef05db4349c611a63
oprinstr <oprinstr@D360-S19>
2025-05-09 15:57:11 +0200

    ajout ChargeJson

--- algo_SAE/Program.cs
+++ algo_SAE/Program.cs
@@ -50,6 +50,40 @@ namespace algo_SAE
             return nb;
         }
 
+        public class ChargeJson
+        {
+            public static Dictionary<T, double> ChargeDictionary<T>(String pathName)
+            {
+                Dictionary<T, double> liste = null;
+                try
+                {
+                    String contenuFichier = File.ReadAllText(pathName);
+                    liste = JsonConvert.DeserializeObject<Dictionary<T, double>>(contenuFichier);
+                }
+                catch (Exception) { throw; }
+                return liste;
+            }
+            public static List<T> ChargeList<T>(String pathName)
+            {
+                List<T> liste = null;
+                try
+                {
+                    String contenuFichier = File.ReadAllText(pathName);
+                    liste = JsonConvert.DeserializeObject<List<T>>(contenuFichier);
+                }
+                catch (Exception) { throw; }
+                return liste;
+            }
+            public static void SauvegardeList<T>(List<T> liste, String pathName)
+            {
+                try
+                {
+                    string result = JsonConvert.SerializeObject(liste, Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText(pathName, result);
+                }
+                catch (Exception) { throw; }
+            }
+        }
 
         public void CoutMensuelFix()
         {
