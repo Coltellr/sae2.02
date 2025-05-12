ba613aac97b88fc99cb43cd23f3ae2cd4f40fdcd
oprinstr <oprinstr@D360-S19>
2025-05-12 11:25:46 +0200

    Ajot classe commande

--- algo_SAE/Commande.cs
+++ algo_SAE/Commande.cs
@@ -8,5 +8,54 @@ namespace algo_SAE
 {
     public class Commande
     {
+        private string moisDeCommande;
+        private List<Produit> lesProduits;
+
+        public Commande(string moisDeCommande, List<Produit> lesProduits)
+        {
+            this.MoisDeCommande = moisDeCommande;
+            this.LesProduits = lesProduits;
+        }
+
+        public List<Produit> LesProduits
+        {
+            get
+            {
+                return this.lesProduits;
+            }
+
+            set
+            {
+                this.lesProduits = value;
+            }
+        }
+
+        public string MoisDeCommande
+        {
+            get
+            {
+                return this.moisDeCommande;
+            }
+
+            set
+            {
+                this.moisDeCommande = value;
+            }
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override string? ToString()
+        {
+            string result = "";
+            foreach (Produit produit in this.LesProduits)
+            {
+                result += produit.ToString();
+            }
+            return base.ToString();
+        }
     }
 }
